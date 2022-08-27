d6ed5639e6605d5dd558a5e4b897fcd93a1e0033
John Rhanzel <rhanzel1016@gmail.com>
2022-08-28 00:13:27 +0800

    Create functionParameter.js

---
+++ functionParameters.js
@@ -0,0 +1,6 @@
+function addTwoNumbes(num1, num2) {
+    let sumOfTwoNumbers = num1 + num2;
+    console.log(sumOfTwoNumbers);
+}
+//display output by calling function and indicating numbers for parameters
+addTwoNumbes(13, 42);
