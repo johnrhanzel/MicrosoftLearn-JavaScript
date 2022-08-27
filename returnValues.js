03ab4bc9fe2b72dc498c999a14fbe034fe9e5e7d
John Rhanzel <rhanzel1016@gmail.com>
2022-08-28 00:16:48 +0800

    Create returnValue.js

---
+++ returnValues.js
@@ -0,0 +1,7 @@
+function addTwoNumbes(num1, num2 = 3.14) {
+    let sumOfTwoNumbers = num1 + num2;
+    //similar to console.log for this program
+    return(sumOfTwoNumbers);
+}
+//display output by calling the function and indicating a number for the first parameter
+addTwoNumbes(16);
